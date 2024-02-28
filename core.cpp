64b566c5fd1945fa827ad26b0c57b9fe483d6cb4
SyS <sreyassreelal@gmail.com>
2024-02-28 15:12:58 +0530

    format code

--- core.cpp
+++ core.cpp
@@ -5,9 +5,9 @@ OMPRSCore::OMPRSCore(const std::string& gamemode_name)
 #if _WIN32
 	proc_handle = LoadLibrary(("omprsgamemode\\" + gamemode_name + ".dll").c_str());
 #else
-	// overwrite dlopen flags set by open.mp 
-	dlopen("components/omprs.so",RTLD_GLOBAL|RTLD_LAZY);
-	
-	proc_handle = dlopen(("omprsgamemode/" + gamemode_name + ".so").c_str(),RTLD_GLOBAL|RTLD_NOW);
+	// overwrite dlopen flags set by open.mp
+	dlopen("components/omprs.so", RTLD_GLOBAL | RTLD_LAZY);
+
+	proc_handle = dlopen(("omprsgamemode/" + gamemode_name + ".so").c_str(), RTLD_GLOBAL | RTLD_NOW);
 #endif
 }
