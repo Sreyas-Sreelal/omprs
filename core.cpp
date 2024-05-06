df95f4c17b9ff3e5152e02d8ee00880cc9bc99fa
SyS <sreyassreelal@gmail.com>
2024-05-06 17:45:10 +0530

    use gamemodes directory for gamemodes

--- core.cpp
+++ core.cpp
@@ -3,12 +3,12 @@
 OMPRSCore::OMPRSCore(const std::string& gamemode_name)
 {
 #if _WIN32
-	proc_handle = LoadLibrary(("omprsgamemode\\" + gamemode_name + ".dll").c_str());
+	proc_handle = LoadLibrary(("gamemodes\\" + gamemode_name + ".dll").c_str());
 #else
 	// overwrite dlopen flags set by open.mp
 	component_handle = dlopen("components/omprs.so", RTLD_GLOBAL | RTLD_NOW);
 
-	proc_handle = dlopen(("omprsgamemode/" + gamemode_name + ".so").c_str(), RTLD_GLOBAL | RTLD_NOW);
+	proc_handle = dlopen(("gamemodes/" + gamemode_name + ".so").c_str(), RTLD_GLOBAL | RTLD_NOW);
 #endif
 }
 
